09d47de4342af5d4d86717766008c00a34024704
ai-244-rusakov <9252293@stud.op.edu.ua>
2025-10-22 20:05:44 +0300

    Update Main.java

--- list/src/main/java/list/Main.java
+++ list/src/main/java/list/Main.java
@@ -2,8 +2,8 @@ package ua.opnu.list;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("REX");
-        Cat cat = new Cat("Mittens");
+        Dog dog = new Dog("Khabib Nurmagomedov");
+        Cat cat = new Cat("Conor Mcgregor");
 
         System.out.println(dog.getName() + " Saying: " + dog.makeSound());
         System.out.println(cat.getName() + " Saying: " + cat.makeSound());
